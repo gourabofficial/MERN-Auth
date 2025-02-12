1d024f7b5c953440084a81f24c804c4b34b5d0e9
gourabofficial <gangulygourab3@gmail.com>
2025-02-13 00:03:25 +0530

    update allowedOrigins to include FRONTEND_PORT from environment variables

--- Backend/app.js
+++ Backend/app.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = [process.env.FRONTEND_PORT , "http://localhost:5173"];
 
 app.use(express.json());
 app.use(cors({origin: allowedOrigins, credentials: true}));
