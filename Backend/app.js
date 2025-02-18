1a9f42df93a206b931482e41fe30c4418190c746
gourabofficial <gangulygourab3@gmail.com>
2025-02-18 21:02:29 +0530

    refactor CORS middleware to enhance origin validation and add request logging

--- Backend/app.js
+++ Backend/app.js
@@ -17,8 +17,26 @@ const allowedOrigins = [
 ];
 
 app.use(express.json());
-app.use(cors({origin: allowedOrigins, credentials: true}));
+// app.use(cors({origin: allowedOrigins, credentials: true}));
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
 app.use(cookieParser());
+
+// debug
+app.use((req, res, next) => {
+  console.log(`Incoming request from: ${req.headers.origin}`);
+  next();
+});
 
 //API Endpoints
 
